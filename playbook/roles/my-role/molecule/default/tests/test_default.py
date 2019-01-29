745a4bd9398e73031cfa7b949edc3df043128f5f
chris.ottinger <chris.ottinger@dpc.vic.gov.au>
2019-01-29 23:18:38 +1100

    role now using pre-existing rg and vnet

--- playbook/roles/my-role/molecule/default/tests/test_default.py
+++ playbook/roles/my-role/molecule/default/tests/test_default.py
@@ -1,14 +1,33 @@
-import os
+# import os
+import pytest
 
-import testinfra.utils.ansible_runner
+# import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+# testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+#     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+# expected installed packages
+@pytest.mark.parametrize('package', [
+  ('nginx'),
+])
+def test_installed_packages(host, package):
+    pkg = host.package(package)
+    assert pkg.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+# expected services are running and enabled
+@pytest.mark.parametrize('service', [
+    ('nginx'),
+])
+def test_running_services(host, service):
+    svc = host.service(service)
+    assert svc.is_running
+    assert svc.is_enabled
+
+# expected ports listening
+# note that is a test of port binding, not firewall/network security group rules
+@pytest.mark.parametrize('socket', [
+    ('tcp://0.0.0.0:80'),
+])
+def test_port_bindings(host, socket):
+    assert host.socket(socket).is_listening
